3069217bd38721f74260a620a9fbac974580213f
Arturo <a245329@uach.mx>
2015-11-17 23:46:52 -0700

    2

--- app/models/Post.java
+++ app/models/Post.java
@@ -138,9 +138,6 @@ public class Post extends Model {
      * Elimina comentarios
      */
     @OnDelete void cascadeDelete() {
-//        comments.stream().forEach((c) -> {
-//            c.delete();
-//        });
     }
 
 }
